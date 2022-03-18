4d007168e087eedffe9233ebfa65f6e098666695
Tsz San Sam, CHAN <samchan148@gmail.com>
2022-03-18 18:16:06 +0800

    fix: add init checking

--- src/container.ts
+++ src/container.ts
@@ -3,7 +3,10 @@ import type { Module } from "./types";
 export async function importModule<T>(dynamicIocModule: Promise<Module<T>>) {
   const dynamicMod = await dynamicIocModule;
 
-  if (dynamicMod.__init__) await dynamicMod.__init__();
+  if (dynamicMod.__init__ && !dynamicMod.__init__.loaded) {
+    await dynamicMod.__init__();
+    dynamicMod.__init__.loaded = true;
+  }
 
   const { __init__, ...mod } = dynamicMod;
 
@@ -16,7 +19,10 @@ export async function importModule<T>(dynamicIocModule: Promise<Module<T>>) {
  * please make sure the `__init__` function is also sync
  */
 export function importModuleSync<T>(iocModule: Module<T>) {
-  if (iocModule.__init__) iocModule.__init__();
+  if (iocModule.__init__ && !iocModule.__init__.loaded) {
+    iocModule.__init__();
+    iocModule.__init__.loaded = true;
+  }
 
   const { __init__, ...mod } = iocModule;
 
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,2 @@
-export type Module<T> = T & { __init__?: () => Promise<void> };
+export type Module<T> = T & { __init__?: { (): Promise<void>; loaded?: boolean } };
 export type IoCModule<T> = Omit<Module<T>, "__init__">;
