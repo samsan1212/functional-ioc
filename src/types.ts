061866370292ae9f82622c32bb4013667602ed3a
Tsz San Sam, CHAN <samchan148@gmail.com>
2022-03-18 21:09:44 +0800

    fix: fix Module type

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,8 @@
-export type Module<T> = T & { __init__?: { (): Promise<void>; loaded?: boolean } };
+export type Module<T> = T & {
+  __init__?: {
+    (): Promise<void> | void;
+    loaded?: boolean;
+  };
+};
+
 export type IoCModule<T> = Omit<Module<T>, "__init__">;
